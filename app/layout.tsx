b8085910418c68b8e71b9fa64a68405ad6aca826
kristina <kristinastefanelli@gmail.com>
2024-02-07 09:11:07 -0500

    feat: prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Chat App",
+  title: 'Chat App',
   description:
-    "Chat app that translates languages allowing you chat with users who speak a different language.",
+    'Chat app that translates languages allowing you chat with users who speak a different language.',
 };
 
 export default function RootLayout({
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
