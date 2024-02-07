54d0ec6c6e46b1b3ca84a2dd4f9889428a8c9570
kristina <kristinastefanelli@gmail.com>
2024-02-07 09:55:16 -0500

    feat: homepage

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Logo from './Logo';
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 bg-white dark:bg-gray-900">
+      <nav className="flex flex-col sm:flex-row items-center p-5 pl-2 bg-white dark:bg-gray-900 max-w-7xl mx-auto">
+        <Logo />
+        <div className="flex-1 flex items-center justify-end space-x-4"></div>
+      </nav>
+    </header>
+  );
+}
