2f2cc7137ac6f66bbece84f0cd48f3a88c2ce060
Kristina Stefanelli <kristinastefanelli@gmail.com>
2024-02-12 07:00:11 -0500

    feat: navigation (#3)
    * feat: navigation
    
    * feat: implement dark/light mode
    
    * feat: user button/avatar
    
    * feat: user button
    
    * Trigger Vercel deployment

---
+++ components/Logo.tsx
@@ -0,0 +1,25 @@
+import LogoImage from '@logos/chat.svg';
+import Link from 'next/link';
+import { AspectRatio } from './ui/aspect-ratio';
+import Image from 'next/image';
+
+export default function Logo() {
+  return (
+    <Link href="/" prefetch={false} className="overflow-hidden">
+      <div className="flex items-center w-72 h-14">
+        <AspectRatio
+          ratio={16 / 9}
+          className="flex items-center justify-center pb-2 md:pb-0"
+        >
+          <Image
+            priority
+            src={LogoImage}
+            alt="logo"
+            className="dark:filter dark:invert w-16 h-16"
+          />
+          Chat
+        </AspectRatio>
+      </div>
+    </Link>
+  );
+}
