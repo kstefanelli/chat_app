ec84be166b9b09e43b5bdd322cf736239de89f7c
Kristina Stefanelli <kristinastefanelli@gmail.com>
2024-02-22 10:14:39 -0500

    feat: chats page (#15)

---
+++ app/(user)/chat/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type Props = {
+  params: {};
+  searchParams: {
+    error: string;
+  };
+};
+
+function ChatsPage({ searchParams: { error } }: Props) {
+  return <div>{/* <ChatList /> */}</div>;
+}
+
+export default ChatsPage;
--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -3,14 +3,44 @@
 import { useRouter } from 'next/navigation';
 import { Button } from './ui/button';
 import { MessageSquarePlusIcon } from 'lucide-react';
+import { useSession } from 'next-auth/react';
+import { useState } from 'react';
+import { useToast } from './ui/use-toast';
+// import LoadingSpinner from "./LoadingSpinner"
+import { v4 as uuidv4 } from 'uuid';
 
-function CreateChatButton() {
+function CreateChatButton({ isLarge }: { isLarge?: boolean }) {
+  const { data: session } = useSession();
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const { toast } = useToast();
+  // const subscription = useSubscriptionStore((state: any) => state.subscription);
 
   const CreateNewChat = async () => {
-    // add logic
+    if (!session?.user.id) return;
+
+    setLoading(true);
+    toast({
+      title: 'Creating new chat...',
+      description: 'Hold tight while we create your new chat...',
+      duration: 3000,
+    });
+
+    const chatId = uuidv4();
+
     router.push(`/chat/abc`);
   };
+
+  if (isLarge) {
+    return (
+      <div>
+        <Button>
+          {/* {loading ? <LoadingSpinner /> : 'Create New Chat' } */}
+        </Button>
+      </div>
+    );
+  }
+
   return (
     <Button onClick={CreateNewChat} variant={'ghost'}>
       <MessageSquarePlusIcon className="text-black dark:text-white" />
